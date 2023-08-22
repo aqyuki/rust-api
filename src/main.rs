bea23b89a438bddaa9d34fc60b507f2470000d80
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-22 15:15:53 +0900

    chroe : レスポンスを返す機能を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
 use std::net::TcpListener;
 
 fn main() {
@@ -5,6 +7,18 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection established!")
+        println!("Connection established!");
+
+        handle_connection(stream)
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
